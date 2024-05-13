4117ea5eaf881201bdb4ec2376e0674c17c7cbc2
Charlee2543 <Charlee2543@hotmail.com>
2024-05-13 16:20:26 +0700

    Ex1-flowchart

---
+++ Ex1_flowchart.js
@@ -0,0 +1,18 @@
+const Ex1_flowchart=20;
+    if(Ex1_flowchart===0){
+    console.log("sunday");}
+    else if(Ex1_flowchart===1){
+    console.log("monday");}
+    else if(Ex1_flowchart===2){
+        console.log("tuesday");}
+        else if(Ex1_flowchart===3){
+            console.log("wednesday");}
+            else if(Ex1_flowchart===4){
+                console.log("thursday");}
+                else if(Ex1_flowchart===5){
+                    console.log("friday");}
+                    else if(Ex1_flowchart===6){
+                        console.log("saturday");}
+                        else{
+                            console.log("Invalid day number");
+                        }
